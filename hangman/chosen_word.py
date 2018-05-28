2696bd1dbc29de598ec1b8edbab67507bb2a0540
NateJH <inatejh@gmail.com>
2018-05-27 20:00:42 -0400

    Revisted for first time in monthes, refactored all files, added central game class to control game

--- hangman/chosen_word.py
+++ hangman/chosen_word.py
@@ -1,50 +1,51 @@
-import random 
+import random
+
 
 class ChosenWord():
-	"""The randomly chosen word the user tries to guess letter by letter"""
-	
-	def __init__(self):
-		"""Randomly selects a word from a dicitonary."""
-		self.selected_word = random.choice(list(open('dictionary.txt')))
-		
-		"""Creates a placeholder to represent letters in the 
-		selected_word the user hasn't yet correctly guesses."""
-		self.proxy_word = []
-		for char in self.selected_word:
-			self.proxy_word.append('*')
-	
-	def dis_proxy_word(self):
-		"""Displays the proxy_word and instructions."""
-		
-		print("Guess letters to try and fill in the word!")
-		print(self.proxy_word)
-		
-	def correct_guess(self, letter):
-		"""If the user's guess is in the selected_word, reveals 
-		corresponding letters in the proxy_word."""
-		
-		if letter in self.selected_word:
-			for pos, char in enumerate(list(self.selected_word)):
-				if char == letter:
-					self.proxy_word[pos] = letter 
-			return self.proxy_word
-			
-	def update_wrong_guesses(self, letter, num):
-		"""If user's guess ins't in the selected_word, adds 1 to 
-		the number of wrong_guesses."""
-		
-		if letter not in self.selected_word:
-			num += 1
-		return num
-			
-	def game_over(self, num):
-		"""If the user's guessed wrong six times, user loses. 
-		If user guessed all the letters in selected_word, user wins"""
-		
-		if num == 6:
-			print("You lost")
-			quit()
-		elif '*' not in self.proxy_word:
-			print("You win")
-			quit()
+    # The randomly chosen word the user tries to guess letter by letter
 
+    def __init__(self):
+        # Randomly selects a word from the dictionary file
+        self.selected_word = random.choice(list(open('dictionary.txt')))
+
+        # Creates a placeholder to represent letters in the selected_word the user hasn't yet correctly guessed
+        self.proxy_word = []
+        for char in self.selected_word:
+            self.proxy_word.append('*')
+
+        # Number of wrong_guesses the player has entered
+        self.wrong_guesses = 0
+
+    # Displays the proxy_word and instructions to the user
+    def display_proxy_word(self):
+        print("Guess letters to try and fill in the word!")
+        print(self.proxy_word)
+        print("Wrong guesses: " + str(self.wrong_guesses))
+
+    # If the user's guess is in the selected_word, reveals corresponding letters in the proxy_word
+    # If user's guess ins't in the selected_word, adds 1 to the number of wrong_guesses.
+    def update(self, letter):
+        if letter in self.selected_word:
+            for pos, char in enumerate(list(self.selected_word)):
+                if char == letter:
+                    self.proxy_word[pos] = letter
+        else:
+            self.wrong_guesses += 1
+
+    # Returns true if one of conditions for the game ending has been reached
+    def is_game_over(self):
+        return self.wrong_guesses >= 6 or '*' not in self.proxy_word
+
+    # If the user's guessed wrong six times, user loses. If user guessed all the letters in selected_word, user wins.
+    def game_over_condition(self):
+        if self.wrong_guesses == 6:
+            print("You lost!")
+        elif '*' not in self.proxy_word:
+            print("You win!")
+
+        print("The word was " + str(self.selected_word) + "!")
+        quit()
+
+    # Returns wrong_guesses
+    def get_wrong_guesses(self):
+        return self.wrong_guesses
--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,34 +1,31 @@
 class Hangman():
-	
-	def __init__(self):
-		"""Creates the starting structure of a hangman"""
-		
-		self.head = "  O  "
-		self.midbody = "/ | \\"
-		self.legs = " / \\ "
-		
-	def display_hangman(self, value):
-		"""Displays parts of the hangman depending on 
-		the value of wrong_guesses"""
-		
-		if value == 0:
-			print(self.head)
-			print(self.midbody)
-			print(self.legs)
-		elif value == 1:
-			print(self.head)
-			print(self.midbody)
-			print(self.legs[0:2])
-		elif value == 2:
-			print(self.head)
-			print(self.midbody)
-		elif value == 3:
-			print(self.head)
-			print(self.midbody[0:4])
-		elif value == 4:
-			print(self.head)
-			print(" " + self.midbody[1:3])
-		elif value == 5:
-			print(self.head)
-			
+    # Representation of a hangman as a string
 
+    def __init__(self):
+
+        self.head = "  O  "
+        self.midbody = "/ | \\"
+        self.legs = " / \\ "
+
+    # Displays parts of the hangman depending on the value of wrong_guesses
+    def display_hangman(self, value):
+
+        if value == 0:
+            print(self.head)
+            print(self.midbody)
+            print(self.legs)
+        elif value == 1:
+            print(self.head)
+            print(self.midbody)
+            print(self.legs[0:2])
+        elif value == 2:
+            print(self.head)
+            print(self.midbody)
+        elif value == 3:
+            print(self.head)
+            print(self.midbody[0:4])
+        elif value == 4:
+            print(self.head)
+            print(" " + self.midbody[1:3])
+        elif value == 5:
+            print(self.head)
